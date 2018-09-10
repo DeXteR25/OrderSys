257660ce4239dca141a5312a11ecb26fcb18edca
DeXteR25 <sujanbantawa25@gmail.com>
2018-09-10 17:34:42 +0545

    Update
    Add Cook and main page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router , Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
+import Main from './pages/Main';
+import Cook from './pages/Cook';
 class App extends Component {
   render() {
     return (
       
       <Router>
       <div>
+        <Route  path= "/" component={Main} exact/>
         <Route  path= "/Home" component={Home} exact/>
         <Route  path= "/Menu" component={Menu} exact/>
+        <Route  path= "/Cook" component={Cook} exact/>
       </div>
     </Router>
     
