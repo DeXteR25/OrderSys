76e72a59d8dbb704ecc39603cf23febb348e1380
DeXteR25 <sujanbantawa25@gmail.com>
2018-09-23 17:35:31 +0545

    Add via Sujan
    Menu and count added

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -2,41 +2,84 @@ import React, { Component } from 'react';
 import './Menu.css';
 import { Link } from 'react-router-dom';
 import { BrowserRouter as Router , Route } from 'react-router-dom';
+
+// const data = ''
 class Menu extends Component {
+
+    constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true, data:'', clicks: 0, value: 0};
+   
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+//   handleClick() {
+//     this.setState(state => ({
+//       isToggleOn: !state.isToggleOn,
+//       data: state.name
+//     }));
+//     console.log(data);
+//   }
+handleClick(event){
+
+    this.setState((prevState) => ({
+        clicks: prevState.clicks + 1
+   }));
+
+    this.setState({
+        
+        data: event.target.value
+    });
+}
+
+
+
     render(){
       
         
         return(
-            
+              
                 <div className="container">
                     <nav className="navbar navbar-light bg-light">
                     <Link className=" btn" to="/Home" >Back</Link>
                         <h1>Orders List</h1>
-                        <p></p>
+                       
                     </nav>
                     <Router>
                     <div>
-                    <Route  path= "/" render={ () => { return ("Hello");}}/>
+                    <Route  path= "/" render={ () => { return ("");}}/>
                     </div>
                     </Router>
                     <div className="row">
-                                <div className="container panelBody">
-                                        <input type="text" id="inputData" className="inputCss form-control" />
-                                        
+                                <div className="container panelBody"  >
+                                        <div className="orderView" type="text" id="inputData">
+                                       
+                                        {this.state.data}  
+                                        {this.state.clicks}
+                                </div>
+
                                     <div className="rowPad">
-                                        <button className="btn  btn-lg" onClick="getData('Mo:Mo')" >Mo:Mo</button>
-                                        <button className="btn  btn-lg" onClick="getData('Chowmin')">Chowmin</button>
-                                        <button className="btn  btn-lg" onClick="getData('Burger')">Burger</button>
-                                        <button className="btn  btn-lg" onClick="getData('Cola')">Cola</button>
-                                        <button className="btn  btn-lg" onClick="getData('pepsi')">Pepsi</button>
+
+                                  
+                                        <button className="btn  btn-lg" onClick={this.handleClick.bind(this)} value="Mo:Mo"> MO:MO</button>
+                                        <button className="btn  btn-lg" onClick={this.handleClick.bind(this)} value="Chowmin" >CHOWMIN</button>
+                                        <button className="btn  btn-lg" onClick={this.handleClick.bind(this)} value="Burger" >BURGER</button>
+                                        <button className="btn  btn-lg" onClick={this.handleClick.bind(this)} value="Cola" >COLA</button>
+                                        <button className="btn  btn-lg" onClick={this.handleClick.bind(this)} value="Pepsi" >PEPSI</button>
                                     </div>
                                    
-                                   
+                                   {/* When button clicks it render data from button.value using event */}
+                                    
                                    
                                     <div class="rowPad">
-                                        <button tyepe="submit" className="btn btn-info" onclick="inputValidation()">result</button>
-                                        <button className="btn btn-info" onclick="clearAll()">clear all</button>
+                                        <button type="submit" className="btn btn-info" onclick={this.value}>result</button>
+                                       
+                                       
                                     </div>
+
+                                        
                             </div>
                                     
                     </div>
@@ -49,4 +92,3 @@ class Menu extends Component {
 }
 
 export default Menu;
-
