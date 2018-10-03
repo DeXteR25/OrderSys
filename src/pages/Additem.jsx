58215ddd5ae818a7cad6ad165a4afff3cc9999ad
RaiS4N <santosh984970@gmail.com>
2018-10-03 15:46:05 +0900

    add menu from button
    use onclick with e helps to get trigger the addmenu function

--- src/pages/Additem.jsx
+++ src/pages/Additem.jsx
@@ -2,82 +2,64 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
-  constructor(props){
+   constructor(props) {
     super(props);
 
-    this.state ={
-      buyItems: ['milk','bread','fruit'],
-      message: '',
+    this.state = {
+      buyItems: ["milk", "bread", "fruit"],
+      message: ""
     };
   }
 
-  onClickButton1 = () => {
-    this.setState({
-      text: 'clicked 1'
-    });
-  }
-
-  onClickButton2 = () => {
-    this.setState({
-      text: 'clicked 2'
-    });
-  }
-
-  addItem(e){
-    e.preventDefault();
-    const {buyItems} = this.state;
-    const newItem = this.newItem.value;
+  
+// add order name in the list array 
+  addItem(item) {
+    const { buyItems } = this.state;
+    const newItem = item;
     const isOnTheList = buyItems.includes(newItem);
-    if (isOnTheList){
+    if (isOnTheList) {
       this.setState({
-        message : 'This item is already on the list.'
-      })
-
-    } else{
-      newItem !== '' && this.setState({
-        buyItems: [...this.state.buyItems, newItem],
-        message : ''
-      })
+        message: "This item is already on the list."
+      });
+    } else {
+      newItem !== "" &&
+        this.setState({
+          buyItems: [...this.state.buyItems, newItem],
+          message: ""
+        });
     }
 
-    
-    this.addForm.reset();
+    // this.addForm.reset();
   }
-  removeItem(item){
+
+// remove order name from the list array 
+  removeItem(item) {
     const newBuyItems = this.state.buyItems.filter(buyItems => {
       return buyItems !== item;
     });
     this.setState({
       buyItems: [...newBuyItems]
-    })
+    });
   }
+
+
   render() {
-    const {buyItems, message } =this.state;
+    const { buyItems, message } = this.state;
     return (
-
-     <div className="container">
-       <form ref={input => this.addForm = input } className="form-inline" onSubmit={(e) => {this.addItem(e)}}>
-         <div className="form-group">
-           <label className="sr-only" htmlFor="newItemInput">Add New Item</label>
-           <input ref={input => this.newItem = input } type="text" placeholder="bread" className="form-control" id="newItemInput" />           
-         </div>
-         <button type="submit" className="btn btn-primary">Add</button>
-       </form>
-
-       <div>
-        <button onClick={this.onClickButton1}>
-          Button 1
+      <div className="container">
+        <button type="button" onClick={e => this.addItem("momo")}>
+          Momo
         </button>
-        <button onClick={this.onClickButton2}>
-          Button 2
+        <button type="button" onClick={e => this.addItem("chowmin")}>
+          Chowmin
         </button>
-        <h1>{this.state.text}</h1>
-      </div>
 
-       {
-         message !== '' && <p className="message text-danger">{message}</p>
-       }
-       <div class="col-md-6 col-lg-3 ">
+        <div>
+          <h1>{this.state.text}</h1>
+        </div>
+
+        {message !== "" && <p className="message text-danger">{message}</p>}
+        <div className="col-md-6 col-lg-3 ">
           <table className="table">
             <thead>
               <tr>
@@ -87,28 +69,27 @@ class App extends Component {
               </tr>
             </thead>
             <tbody>
-              {
-                buyItems.map(item => {
-                  return(
-                    <tr key={item}>
+              {buyItems.map(item => {
+                return (
+                  <tr key={item}>
                     <th scope="row">1</th>
                     <td>{item}</td>
                     <td className="text-middle">
-                      <button onClick={(e)=> this.removeItem(item)} type="button" className="btn btn-default btn-sm">
+                      <button
+                        onClick={e => this.removeItem(item)}
+                        type="button"
+                        className="btn btn-default btn-sm"
+                      >
                         Remove
                       </button>
                     </td>
-                    </tr>
-                  ) 
-                })
-              }
-              
-            
+                  </tr>
+                );
+              })}
             </tbody>
-        </table>
+          </table>
+        </div>
       </div>
-     
-     </div>
     );
   }
 }
