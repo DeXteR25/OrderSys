21e651b0bb6f6cad2786c0ff6425ca888296afc2
DeXteR25 <sujanbantawa25@gmail.com>
2018-11-16 10:36:24 +0545

    Update
    Add new pages

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import Home from './pages/Home';
 import Menu from './pages/Menu';
 import Main from './pages/Main';
 import Cook from './pages/Cook';
+import Menuadd from './pages/Menuadd';
+import AddItem from './pages/AddItem';
+import AddTable from './pages/AddTable';
+import Add from './pages/Add';
 class App extends Component {
   render() {
     return (
       
       <Router>
-      <div>
-        <Route  path= "/" component={Main} exact/>
-        <Route  path= "/Home" component={Home} exact/>
-        <Route  path= "/Menu" component={Menu} exact/>
-        <Route  path= "/Cook" component={Cook} exact/>
-      </div>
+          <div>
+            <Route  path= "/Main" component={Main} exact/>
+            <Route  path= "/" component={Home} exact/>
+            <Route  path= "/Menu" component={Menu} exact/>
+            <Route  path= "/Cook" component={Cook} exact/>
+            <Route  path= "/Menuadd" component={Menuadd} exact/>
+            <Route  path= "/AddItem" component={AddItem} exact/>
+            <Route  path= "/AddTable" component={AddTable} exact/>
+            <Route  path= "/Add" component={Add} exact/>
+          </div>
     </Router>
     
     );
@@ -23,3 +31,4 @@ class App extends Component {
 }
 
 export default App;
+
